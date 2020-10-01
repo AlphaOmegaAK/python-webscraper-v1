63a9908b123113e6394269475b6e2425e1234ac8
Artur <alphaunionxt@gmail.com>
2020-10-01 16:35:18 -0400

    Got the title

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from urllib.request import urlopen
 
-url = "https://news.ycombinator.com/e"
+url = "https://news.ycombinator.com//"
 
 page = urlopen(url)
-print(page)
+# print(page)
 
 
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
 
-print(html)
+# print(html)
+
+title_index = html.find('<title>')
+
+# print(title_index)
+
+
+start_index = title_index + len("<title>")
+
+# print(start_index)
+
+end_index = html.find("</title>")
+
+title = html[start_index:end_index]
+print(title)
